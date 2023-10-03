8dc0f66a25d4aa0a4ff07862178ccc8640a06e32
Igor-Si-Unimontes <igorunimoc@gmail.com>
2023-10-02 23:49:08 -0300

    Atualização api

--- coletaDados.php
+++ coletaDados.php
@@ -8,7 +8,7 @@ print($_POST['name']."<br>");
 
 print("Ano nascimento:");
 print($_POST['year']."<br>");
-
+/*
 print("CEP:");
 print($_POST['cep']."<br>");
 
@@ -26,7 +26,7 @@ print($_POST['city']."<br>");
 
 print("Estado:");
 print($_POST['state']."<br>");
-
+*/
 print("Celular:");
 print($_POST['number']."<br>");
 
@@ -34,4 +34,24 @@ print("E-mail:");
 print($_POST['email']."<br>");
 
 //parei nos sim e nao
+
+
+$cep = $_POST['cep'];
+
+$retorno = json_decode(file_get_contents("https://viacep.com.br/ws/$cep/json/"));
+
+//var_dump($retorno);
+
+print("<h1>Resultado da consulta</h1>");
+print("Logradouro: ".$retorno->logradouro."<br>");
+
+if($retorno->complemento <> "") {
+   print("Complemento: ".$retorno->complemento."<br>");
+}
+
+
+print("Bairro: ".$retorno->bairro."<br>");
+print("Cidade: ".$retorno->localidade."<br>");
+print("Estado: ".$retorno->uf);
+
 ?>
